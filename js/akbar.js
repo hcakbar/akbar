e7efdb2e42a1e1f4dbaae35ae10e77d64b881416
akbar <hcakbar@gmail.com>
2018-05-27 22:17:16 -0400

    fixing body content

--- js/akbar.js
+++ js/akbar.js
@@ -1,15 +1,3 @@
-// // expand drawer when click
-// var menuIcon = document.querySelector('#menu-icon');
-// var main = document.querySelector('main');
-// var navClass = document.querySelector('.nav');
-// menuIcon.addEventListener('click', function (e) {
-//     navClass.classList.toggle('open');
-//     e.stopPropagation();
-// });
-// main.addEventListener('click', function () {
-//     navClass.classList.remove('open');
-// });
-
 $(document).ready(function() {
     $('.menu-icon').on('click', function() {
         $('header nav ul').toggle('nav-menu');
